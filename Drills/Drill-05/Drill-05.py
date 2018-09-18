367379e89b4a98d07454e123751add4e99c76a0c
gnu <rlarmsdn321@naver.com>
2018-09-18 18:25:45 +0900

    Drill-05 v9
    make_circle Done
    main func Done

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -33,7 +33,7 @@ def move_left():
         delay(0.01)
         
 def move_down():
-    x, y=0+25, 600-25
+    x, y=0+25, 600-50
     while(y > 0+90):
         clear_canvas_now()
         grass.draw_now(400, 30)
@@ -56,9 +56,20 @@ def make_rectangle():
     move_left()
     move_down()
     move_left2center()
+
+import math
     
 def make_circle():
-    pass
+    cx, cy, r = 800//2, 600//2, (600-180)//2
+    degree = -90
+    while(degree < 360 - 90):
+        radian = math.radians(degree)
+        x,y=cx+r*math.cos(radian), cy+r*math.sin(radian)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x,y)
+        degree += 1
+        delay(0.01)
 
 while True:
     make_rectangle()
