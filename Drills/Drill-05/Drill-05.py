6f14400984d4603aa178fb411c5df526ddbf7c4c
gnu <rlarmsdn321@naver.com>
2018-09-18 18:35:06 +0900

    R_Drill-05 v2
    Position Update

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -6,6 +6,8 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 def move_rotation():
+    Position=[(203, 535), (132, 243), (535, 470), (477, 203), (715, 136), (316, 225), (510, 92), (692, 518), (682, 336), (712, 349)]
+
     pass
 
 while True:
