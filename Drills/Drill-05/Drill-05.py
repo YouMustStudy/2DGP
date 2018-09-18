1bdb5c6d7c9db8c5d54c0512a2c7c2c29dd3704d
gnu <rlarmsdn321@naver.com>
2018-09-18 18:51:04 +0900

    R_Drill-05 v5
    Apply Double buffering
    Change Image Set for Direction

--- Drills/Drill-05/Drill-05.py
+++ Drills/Drill-05/Drill-05.py
@@ -3,11 +3,13 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
-move_count=10
 
 def move_rotation():
+    move_count=10
+    frame=0
+    Direction=1
     Position=[(203, 535), (132, 243), (535, 470), (477, 203), (715, 136), (316, 225), (510, 92), (692, 518), (682, 336), (712, 349)]
     for num in range(0, 10):
         x,y=Position[num]
@@ -19,9 +21,11 @@ def move_rotation():
             x2,y2=Position[0]
         dx,dy=(x2-x1)/move_count,(y2-y1)/move_count
         for count in range(0, move_count):
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
+            clear_canvas()
+            grass.draw(400, 30)
+            character.clip_draw(frame * 100, (Direction)*100, 100, 100, x, y)
+            update_canvas()
+            frame=(frame+1)%8
             x+=dx
             y+=dy
             delay(0.01)
