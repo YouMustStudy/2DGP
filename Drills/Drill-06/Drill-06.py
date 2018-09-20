3a41845c27ba568cd98b4ed771e66f0560528c31
Gnu_Class <rlarmsdn321@naver.com>
2018-09-20 18:05:04 +0900

    drill-06 v4
    변위를 구하는 함수 추가

--- Drills/Drill-06/Drill-06.py
+++ Drills/Drill-06/Drill-06.py
@@ -2,6 +2,8 @@ from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
+def GetDxDy(a, b)
+    pass
 
 def handle_events():
     global running
@@ -13,6 +15,9 @@ def handle_events():
             running = False
         elif event.type == SDL_MOUSEMOTION:
             px,py=event.x,KPU_HEIGHT-event.y-1
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == SDL_BUTTON_LEFT:
+                GetDxDy(event.x, event.y)
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
@@ -21,12 +26,16 @@ pointer = load_image('hand_arrow.png')
 
 running = True
 px, py = 0, 0
+x,y=KPU_WIDTH // 2, KPU_HEIGHT // 2
+dx,dy=0, 0
+count=5
 frame = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    character.clip_draw(frame * 100, 100, 100, 100, x, y)
     pointer.draw(px+25,py-25)
     update_canvas()
     frame = (frame + 1) % 8
