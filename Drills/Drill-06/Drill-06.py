bf83de330b3fb7ead35ce3bbe80db55b571f5d92
Gnu_Class <rlarmsdn321@naver.com>
2018-09-20 17:54:27 +0900

    Drill-06 v2
    마우스 포인터 추가

--- Drills/Drill-06/Drill-06.py
+++ Drills/Drill-06/Drill-06.py
@@ -4,21 +4,30 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
 def handle_events():
-    pass
+    global running
+    global px,py
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEMOTION:
+            px,py=event.x,KPU_HEIGHT-event.y-1
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
+pointer = load_image('hand_arrow.png')
 
 running = True
-x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+px, py = 0, 0
 frame = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    pointer.draw(px,py)
     update_canvas()
     frame = (frame + 1) % 8
 
