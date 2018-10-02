fbf520a57ea1de5e07996990da99fde627773a21
YouMustStudy <YMS>
2018-10-02 18:35:21 +0900

    Drill-07 v4
    좌표에 따른 무한이동 구현

--- Drills/Drill-07/Drill-07.py
+++ Drills/Drill-07/Drill-07.py
@@ -1,10 +1,5 @@
 from pico2d import *
 import random
-KPU_WIDTH, KPU_HEIGHT = 1280, 1024
-
-open_canvas(KPU_WIDTH, KPU_HEIGHT)
-kpu_ground = load_image('KPU_GROUND.png')
-character = load_image('animation_sheet.png')
 
 def GetLine(p1, p2, vertex_num):
     dx = 100//vertex_num
@@ -18,16 +13,29 @@ def MakeRandomPosition(width, height, size):
     points=[(random.randint(0, width), random.randint(0, height)) for i in range(size)]
     return points
 
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
-points=MakeRandomPosition(KPU_WIDTH, KPU_HEIGHT, 2)
+
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+kpu_ground = load_image('KPU_GROUND.png')
+character = load_image('animation_sheet.png')
+
+MAX_VERTEX=10
+LINE_VERTEX=20
 frame=0
-line=GetLine(points[0], points[1], 10)
-count=0
+Vertex_Index=0
+Line_Index=0
+
+points=MakeRandomPosition(KPU_WIDTH, KPU_HEIGHT, MAX_VERTEX)
 while True:
-    clear_canvas()
-    kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100, 100, 100, line[count][0],line[count][1])
-    update_canvas()
-    delay(0.01)
-    frame = (frame + 1) % 8
-    count = (count + 1) % 10
+    line=GetLine(points[Vertex_Index], points[(Vertex_Index+1)%MAX_VERTEX], LINE_VERTEX)
+    for i in range(LINE_VERTEX-1):
+        clear_canvas()
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        character.clip_draw(frame * 100, 100, 100, 100, line[Line_Index][0], line[(Line_Index+1)%LINE_VERTEX][1])
+        update_canvas()
+        delay(0.02)
+        frame = (frame + 1) % 8
+        Line_Index = (Line_Index + 1) % LINE_VERTEX
+    Line_Index=0
+    Vertex_Index = (Vertex_Index + 1) % MAX_VERTEX
