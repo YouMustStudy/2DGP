eaf06eeb1f2e61b3ddc423a3068de593f3563d46
YouMustStudy <YMS>
2018-10-02 18:45:54 +0900

    Drill-07 v5
    전체 정점간 경로를 구해 리스트로 반환하는 함수 구현

--- Drills/Drill-07/Drill-07.py
+++ Drills/Drill-07/Drill-07.py
@@ -1,20 +1,25 @@
 from pico2d import *
 import random
 
-def GetLine(p1, p2, vertex_num):
-    dx = 100//vertex_num
+def GetLine(p1, p2, line_num):
+    dx = 100//line_num
     line=[]
     for i in range(0, 100+1, dx):
         t=i/100
         line.append(((1-t)*p1[0] + t*p2[0], (1-t)*p1[1] + t*p2[1]))
     return line
 
+def GetAllLine(Vlist, vertex_num, line_num):
+    AllLine=[]
+    for i in range (vertex_num):
+        AllLine.append(GetLine(Vlist[i], Vlist[(i+1)%vertex_num], line_num))
+    return AllLine
+
 def MakeRandomPosition(width, height, size):
     points=[(random.randint(0, width), random.randint(0, height)) for i in range(size)]
     return points
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
-
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
@@ -27,12 +32,12 @@ Vertex_Index=0
 Line_Index=0
 
 points=MakeRandomPosition(KPU_WIDTH, KPU_HEIGHT, MAX_VERTEX)
+AllLine=GetAllLine(points, MAX_VERTEX, LINE_VERTEX)
 while True:
-    line=GetLine(points[Vertex_Index], points[(Vertex_Index+1)%MAX_VERTEX], LINE_VERTEX)
     for i in range(LINE_VERTEX-1):
         clear_canvas()
         kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-        character.clip_draw(frame * 100, 100, 100, 100, line[Line_Index][0], line[(Line_Index+1)%LINE_VERTEX][1])
+        character.clip_draw(frame * 100, 100, 100, 100, AllLine[Vertex_Index][Line_Index][0],AllLine[Vertex_Index][Line_Index][1])
         update_canvas()
         delay(0.02)
         frame = (frame + 1) % 8
