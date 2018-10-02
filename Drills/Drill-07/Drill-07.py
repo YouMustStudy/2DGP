59bf5f2d7c059e8430b589c16ff55a7f964425c0
YouMustStudy <YMS>
2018-10-02 17:53:37 +0900

    Drill-07 v2
    랜덤 좌표 설정함수 완성

--- Drills/Drill-07/Drill-07.py
+++ Drills/Drill-07/Drill-07.py
@@ -5,7 +5,6 @@ import random
 def stop():
     turtle.bye()
 
-
 def prepare_turtle_canvas():
     turtle.setup(1024, 768)
     turtle.bgcolor(0.2, 0.2, 0.2)
@@ -43,27 +42,41 @@ def draw_big_point(p):
     turtle.color(0.8, 0.9, 0)
     turtle.dot(15)
     turtle.write('     '+str(p))
-
-
+    
 def draw_point(p):
     turtle.goto(p)
     turtle.dot(5, random.random(), random.random(), random.random())
 
-
 def draw_line_basic(p1, p2):
+    draw_big_point(p1)
+    draw_big_point(p2)
     x1, y1=p1[0], p1[1]
     x2, y2=p2[0], p2[1]
-    a=
-    pass
-
+    a=(y2-y1)/(x2-x1)
+    b=y1-a*x1
+    for x in range(x1, x2+1, 2):
+        y=a*x+b
+        draw_point((x,y))
+    draw_point(p2)
 
 def draw_line(p1, p2):
-    # fill here
-    pass
+    draw_big_point(p1)
+    draw_big_point(p2)
 
+    for i in range(0, 100+1, 10):
+        t=i/100
+        x=(1-t)*p1[0] + t*p2[0]
+        y=(1-t)*p1[1] + t*p2[1]
+        draw_point((x,y))
+    
+    draw_point(p2)
+
+def MakeRandomPosition(width, height, size):
+    points=[(random.randint(0, width), random.randint(0, height)) for i in range(size)]
+    return points
 
 prepare_turtle_canvas()
 
-# fill here
+draw_line(points[0], points[1])
 
 turtle.done()
