481c6cac5201f32d382143bf78d8631b8878b522
Gnu_InClass <ClassRoom>
2018-10-04 18:35:47 +0900

    Drill-08-01 v4
    2015156002 김근우
    Drill-08-01 커밋횟수 4회
    https://youtu.be/DyjnowvFz8g

--- Drills/Drill-08/Drill-08-2.py
+++ Drills/Drill-08/Drill-08-2.py
@@ -11,7 +11,7 @@ def GetLine(p1, p2, p3, p4):
 def GetAllLine(Vlist, vertex_num):
     AllLine=[]
     for i in range (vertex_num):
-        AllLine.append(GetLine(Vlist[i], Vlist[(i+1)%vertex_num], Vlist[(i+2)%vertex_num], Vlist[(i+3)%vertex_num]))
+        AllLine.append(GetLine(Vlist[(i-1)%vertex_num], Vlist[(i)%vertex_num], Vlist[(i+1)%vertex_num], Vlist[(i+2)%vertex_num]))
     return AllLine
 
 def MakeRandomPosition(width, height, size):
@@ -24,12 +24,12 @@ open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
-MAX_VERTEX=4
+MAX_VERTEX=10
 LINE_VERTEX=50
 frame=0
 Vertex_Index=0
 Line_Index=0
-
+count=0
 points=MakeRandomPosition(KPU_WIDTH, KPU_HEIGHT, MAX_VERTEX)
 AllLine=GetAllLine(points, MAX_VERTEX)
 while True:
@@ -40,10 +40,19 @@ while True:
             direction=0
         clear_canvas()
         kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        for n in range(count):
+            if(points[(n)%MAX_VERTEX][0] < points[(n+1)%MAX_VERTEX][0]):
+                stamp_direction=1
+            else:
+                stamp_direction=0
+            character.clip_draw(100, stamp_direction * 100, 100, 100, points[(n+1)%MAX_VERTEX][0], points[(n+1)%MAX_VERTEX][1])
+
         character.clip_draw(frame * 100, direction*100, 100, 100, AllLine[Vertex_Index][Line_Index][0],AllLine[Vertex_Index][Line_Index][1])
         update_canvas()
         delay(0.02)
         frame = (frame + 1) % 8
         Line_Index = (Line_Index + 1)
     Line_Index=0
+    if(count<MAX_VERTEX):
+        count=(count+1)
     Vertex_Index = (Vertex_Index + 1) % MAX_VERTEX
